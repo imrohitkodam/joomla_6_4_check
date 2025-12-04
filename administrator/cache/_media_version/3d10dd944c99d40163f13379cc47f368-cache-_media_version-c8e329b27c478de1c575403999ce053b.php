9092fd61b5566132a40f91e435552721d98c3418
rohit.kodam <rohit.kodam@tekditechnologies.com>
2025-12-04 11:56:30 +0530

    initial commit

---
+++ administrator/cache/_media_version/3d10dd944c99d40163f13379cc47f368-cache-_media_version-c8e329b27c478de1c575403999ce053b.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:6:"33d6bf";s:6:"output";s:0:"";}
---
+++ components/com_jgive/views/campaign/tmpl/default_playvideo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     JGive
+ * @subpackage  com_jgive
+ *
+ * @author      Techjoomla <extensions@techjoomla.com>
+ * @copyright   Copyright (C) 2009 - 2020 Techjoomla. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+$this->video_params['divId'] = "jgive_video";
+
+JPluginHelper::importPlugin('tjvideo', $this->video_params['plugin']);
+$result = Factory::getApplication()->triggerEvent('onRenderPluginHTML', array($this->video_params));
+
+echo $result[0];
